960c68eb983a5f6105abd6f8cdab069b258e36ed
andreykuluev96 <andreykuluev96@omo.systems>
2020-06-30 14:05:14 +0300

    sds

--- card_recorder.ino
+++ card_recorder.ino
@@ -4,11 +4,27 @@
 // The default Mifare Classic key
 
 uint8_t KEY_DEFAULT_KEYAB[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
 uint8_t KEY_OMO_KEYAB[6] = {0x21, 0x48, 0x55, 0x49, 0x3B, 0x2B};
+
+uint8_t KEY_DEFAULT_KEY1[6] = {0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5};
+uint8_t KEY_DEFAULT_KEY2[6] = {0xB0, 0xB1, 0xB2, 0xB3, 0xB4, 0xB5};
+uint8_t KEY_DEFAULT_KEY3[6] = {0x4D, 0x3A, 0x99, 0xC3, 0x51, 0xDD};
+uint8_t KEY_DEFAULT_KEY4[6] = {0x1a, 0x98, 0x2c, 0x7e, 0x45, 0x9a};
+uint8_t KEY_DEFAULT_KEY5[6] = {0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff};
+uint8_t KEY_DEFAULT_KEY6[6] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+uint8_t KEY_DEFAULT_KEY7[6] = {0xd3, 0xf7, 0xd3, 0xf7, 0xd3, 0xf7};
+uint8_t KEY_DEFAULT_KEY8[6] = {0xb2, 0x10, 0xb2, 0x10, 0xb2, 0x10};
+
+uint8_t *KEY_ARR[9] = {KEY_DEFAULT_KEY1, KEY_DEFAULT_KEY2, KEY_DEFAULT_KEY3, KEY_DEFAULT_KEY4, KEY_DEFAULT_KEY5, KEY_DEFAULT_KEY6, KEY_DEFAULT_KEY7, KEY_DEFAULT_KEY8, KEY_OMO_KEYAB};
+
 static const uint8_t KEY_DEFAULT_KEYAB_WRITE[16] = {0x21, 0x48, 0x55, 0x49, 0x3B, 0x2B, 0xFF, 0x07, 0x80, 0x69, 0x21, 0x48, 0x55, 0x49, 0x3B, 0x2B};
+
 
 void getRandomUserId(void);
 void setUserId(String userId);
+void setUserId(char *userId);
+
 
 uint8_t UserID[48] =  {0}; // 8-4-4-4-12
 uint8_t ndefprefix = NDEF_URIPREFIX_NONE;
@@ -49,6 +65,43 @@ void setup(void) {
 
 
 void loop(void) {
+
+  // #########################################################################################################################
+  //getRandomUserId();
+  //setUserId("df298eec-6cd1-4418-ae63-e796828c17ab");
+  // #########################################################################################################################
+  Serial.print("Enter the UserId: ");
+  String inString;
+  while (Serial.available() == 0) {}
+
+  while (Serial.available() > 0) {
+    int inChar = Serial.read();
+      inString += (char)inChar; 
+      Serial.print(inChar);
+    if (inChar == '\n') {
+      Serial.println();
+      Serial.print("UserId: ");
+      Serial.println(inString);
+      //Serial.print("size of str: ");Serial.println(inString.length());
+      if(inString.length() == 37)
+      {
+        setUserId(inString);
+      }
+      else if(inString.length() == 1)
+      {
+        
+      }
+      else
+      {
+        Serial.println("Invalid userId!");
+      }
+      inString = ""; 
+    }
+  }
+  // #########################################################################################################################
+
+
+  
   uint8_t success;                          // Flag to check if there was an error with the PN532
   uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
   uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
@@ -60,10 +113,6 @@ void loop(void) {
   // 'uid' will be populated with the UID, and uidLength will indicate
   // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
   success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
-
-
-  //setUserId("930c6d7b-ea68-4ddf-9852-1ac7c71c19b1");
-  
 
   if (success) {
     // Display some basic information about the card
@@ -100,8 +149,16 @@ void loop(void) {
 
                 if(!success)
                 {
-                  nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
-                  success = nfc.mifareclassic_AuthenticateBlock (uid, uidLength, currentblock, 1, KEY_OMO_KEYAB);
+                  for(int i=0; i<8; i++)
+                  {
+                    nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+                    success = nfc.mifareclassic_AuthenticateBlock (uid, uidLength, currentblock, 1, KEY_ARR[i]);
+                    
+                    if(success)
+                    {
+                      break;
+                    }
+                  }
                 }
             }
             else
@@ -113,8 +170,16 @@ void loop(void) {
 
                 if(!success)
                 {
-                  nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
-                  success = nfc.mifareclassic_AuthenticateBlock (uid, uidLength, currentblock, 1, KEY_OMO_KEYAB);
+                  for(int i=0; i<9; i++)
+                  {
+                    nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+                    success = nfc.mifareclassic_AuthenticateBlock (uid, uidLength, currentblock, 1, KEY_ARR[i]);
+                    
+                    if(success)
+                    {
+                      break;
+                    }
+                  }
                 }
             }
             
@@ -138,7 +203,8 @@ void loop(void) {
         }
         else
         {
-          getRandomUserId();
+          
+
           
           if(currentblock == 4)
           {
@@ -161,15 +227,11 @@ void loop(void) {
           {
             // прошивка кард айди прошла успешно
             //Serial.println("NDEF URI Record written to sector 1");
-
-            
           }
           else
           {
           
             Serial.println("NDEF Record creation failed! :(");
-
-            
           }
 
           // Authenticated ... we should be able to read the block now
@@ -188,8 +250,12 @@ void loop(void) {
             {
               Serial.print(" ");
             }
-            // Dump the raw data
-            nfc.PrintHexChar(data, 16);
+
+            if(currentblock != 7)
+            {
+              // Dump the raw data
+              nfc.PrintHexChar(data, 16);
+            }
 
             if(currentblock == 7)
             {
@@ -231,11 +297,67 @@ void loop(void) {
   Serial.println("\n\nSend a character to run the mem dumper again!");
   //Serial.flush();
   //while (!Serial.available());
-  /*while (Serial.available()) {
+  while (Serial.available()) {
   Serial.read();
-  }*/
-  //Serial.flush();
+  }
+  Serial.flush();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // "df1ff3ef-5ddd-4145-8e7d-5585f746c0c8"; // 8-4-4-4-12
 void getRandomUserId(void)
@@ -259,6 +381,22 @@ void getRandomUserId(void)
 }
 
 void setUserId(String userId)
+{
+  for(int i=0; i<48; i++)
+  {
+    UserID[i] = 0;
+  }
+  
+  for(int i=0; i<36; i++)
+  {
+    UserID[i] = userId[i];
+    //Serial.print(UserID[i]);
+  }
+}
+
+char tempUserId[36] = {0};
+
+void setUserId(char *userId)
 {
   for(int i=0; i<48; i++)
   {
